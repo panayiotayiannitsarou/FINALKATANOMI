c6aeffaf73ea4b8dbc5508a2593dd5384de4c0bb
panayiotayiannitsarou <panayiotaconstantinou818@gmail.com>
2025-05-23 18:09:46 +0300

    Add files via upload

---
+++ app_main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+import io
+import base64
+
+st.set_page_config(page_title="Κατανομή Μαθητών", layout="wide")
+
+# ...εδώ συνεχίζεται ο υπόλοιπος κώδικας όπως έχει...
+
+# Παράδειγμα δομής για εκκίνηση:
+st.title("Καλωσήρθατε στην εφαρμογή Κατανομής Μαθητών")
